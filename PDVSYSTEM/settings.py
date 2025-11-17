184ffd88ef4d68c04d3da743f06be991fb947b3b
Carlos Maute 💯 <Maute007>
2025-11-17 21:50:37 +0200

    Config: Manter apenas SQLite como database

--- PDVSYSTEM/settings.py
+++ PDVSYSTEM/settings.py
@@ -81,24 +81,12 @@ WSGI_APPLICATION = 'PDVSYSTEM.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
-# Use PostgreSQL on Render, SQLite locally
-if 'DATABASE_URL' in os.environ:
-    import dj_database_url
-    DATABASES = {
-        'default': dj_database_url.config(
-            default=os.environ.get('DATABASE_URL'),
-            conn_max_age=600,
-            conn_health_checks=True,
-            ssl_require=True,
-        )
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
-else:
-    DATABASES = {
-        'default': {
-            'ENGINE': 'django.db.backends.sqlite3',
-            'NAME': BASE_DIR / 'db.sqlite3',
-        }
-    }
+}
 
 
 # Password validation
